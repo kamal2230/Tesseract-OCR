bd6171b894397633c600b20698d31e8f79ec8930
Kamal Khumar <kamalkhumar22@gmail.com>
2020-07-09 18:28:10 +0530

    code

--- code/ocr.py
+++ code/ocr.py
@@ -6,4 +6,12 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd="C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 t=pytesseract.image_to_string(Image.open("Tactii.png"))
 t=t.replace("v ","")
-print(t)
+t=t.replace("Name","name")
+t=t.replace("Definition","definition")
+lis=list((t.split("\n\n")))
+lis[0]=list(lis[0])
+for i in range(len(lis[0])):
+    if(lis[0][i]==" " and lis[0][i+1].isupper()):
+        lis[0][i]="$"
+lis[0]=("".join(lis[0])).split("$")
+print(lis)
