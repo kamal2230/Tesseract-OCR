a530be7ff4dd2ac331d0d3e2eae73ed300314625
Kamal Khumar <kamalkhumar22@gmail.com>
2020-07-09 18:10:56 +0530

    code

--- code/ocr.py
+++ code/ocr.py
@@ -4,4 +4,6 @@ except ImportError:
     import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd="C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-print(pytesseract.image_to_string(Image.open("Tactii.png")))
+t=pytesseract.image_to_string(Image.open("Tactii.png"))
+t=t.replace("v ","")
+print(t)
